b8112cd9637ba4e358e0649efb375082846f5bb4
dnnyftryn <dannyfitriyana@gmail.com>
2024-01-16 17:27:54 +0700

    add scanner

--- resources/views/admin/barang/barang-masuk/create.blade.php
+++ resources/views/admin/barang/barang-masuk/create.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.template.app-master')
 
 @section('context')
-    Input Barang
+    Input Barang Masuk
 @endsection
 
 @section('content')
@@ -21,6 +21,7 @@
         <div class="row">
             <div class="col-12">
                 <div class="card">
+                    <div id="reader" width="600px"></div>
                     <div class="card-header">
                         <h3 class="card-title">Tambah Barang</h3>
                     </div>
@@ -95,4 +96,27 @@
     </section>
     <!-- /.content -->
 
+@endsection
+
+@section('script')
+    <script src="https://unpkg.com/html5-qrcode" type="text/javascript"></script>
+
+    <script>
+        function onScanSuccess(decodedText, decodedResult) {
+        // handle the scanned code as you like, for example:
+        console.log(`Code matched = ${decodedText}`, decodedResult);
+        }
+
+        function onScanFailure(error) {
+        // handle scan failure, usually better to ignore and keep scanning.
+        // for example:
+        console.warn(`Code scan error = ${error}`);
+        }
+
+        let html5QrcodeScanner = new Html5QrcodeScanner(
+        "reader",
+        { fps: 10, qrbox: {width: 250, height: 250} },
+        /* verbose= */ false);
+        html5QrcodeScanner.render(onScanSuccess, onScanFailure);
+    </script>
 @endsection

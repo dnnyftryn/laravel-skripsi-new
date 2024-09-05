a33779f8a6d11e94688bd8a33e557fe4e8cc7db0
dnnyftryn <dannyfitriyana@gmail.com>
2024-09-05 14:41:41 +0700

    feat: add login attempt

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -1,78 +1,100 @@
 <?php
-  
+
 namespace App\Http\Controllers\Auth;
-  
+
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Http\Request;
-  
+use Illuminate\Validation\ValidationException;
+
 class LoginController extends Controller
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Login Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller handles authenticating users for the application and
-    | redirecting them to your home screen. The controller uses a trait
-    | to conveniently provide its functionality to your applications.
-    |
-    */
-  
     use AuthenticatesUsers;
-  
-    /**
-     * Where to redirect users after login.
-     *
-     * @var string
-     */
+
     protected $redirectTo = RouteServiceProvider::HOME;
-  
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
+
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
     }
- 
+
     public function login(Request $request)
     {   
         $input = $request->all();
-     
+
         $this->validate($request, [
             'email' => 'required|email',
             'password' => 'required',
         ]);
-     
-        if(auth()->attempt(array('email' => $input['email'], 'password' => $input['password'])))
-        {
-            if (auth()->user()->type == 'admin') 
-            {
+
+        // Check if the user has too many failed login attempts.
+        if ($this->hasTooManyLoginAttempts($request)) {
+            $this->fireLockoutEvent($request);
+            return $this->sendLockoutResponse($request);
+        }
+
+        if (auth()->attempt(['email' => $input['email'], 'password' => $input['password']])) {
+            $this->clearLoginAttempts($request); // Clear the login attempts if successful
+
+            if (auth()->user()->type == 'admin') {
                 return redirect()->route('admin.index');
-            }
-            else if (auth()->user()->type == 'manager') 
-            {
+            } else if (auth()->user()->type == 'manager') {
                 return redirect()->route('manager.home');
-            }
-            else if (auth()->user()->type == 'user') 
-            {
+            } else if (auth()->user()->type == 'user') {
                 return redirect()->route('user.home');
-            } 
-            else if (auth()->user()->type == 'warehouse') 
-            {
+            } else if (auth()->user()->type == 'warehouse') {
                 return redirect()->route('warehouse.home');
-            }
-            else{
+            } else {
                 return redirect()->route('auth.login');
             }
-        }else{
+        } else {
+            // Increment login attempts if unsuccessful
+            $this->incrementLoginAttempts($request);
+
             return redirect()->route('login')
-                ->with('error','Email-Address And Password Are Wrong.');
+                ->with('error', 'Email-Address And Password Are Wrong.');
         }
-          
+    }
+
+    protected function sendLockoutResponse(Request $request)
+    {
+        $seconds = $this->limiter()->availableIn(
+            $this->throttleKey($request)
+        );
+
+        throw ValidationException::withMessages([
+            'email' => [trans('auth.throttle', ['seconds' => $seconds])],
+        ])->status(429);
+    }
+
+    public function username()
+    {
+        return 'email';
+    }
+
+    protected function hasTooManyLoginAttempts(Request $request)
+    {
+        $maxAttempts = 3; // Max number of attempts
+        return $this->limiter()->tooManyAttempts(
+            $this->throttleKey($request), $maxAttempts
+        );
+    }
+
+    protected function incrementLoginAttempts(Request $request)
+    {
+        $this->limiter()->hit(
+            $this->throttleKey($request), 60 * 15 // Lockout time in seconds (15 minutes)
+        );
+    }
+
+    protected function clearLoginAttempts(Request $request)
+    {
+        $this->limiter()->clear($this->throttleKey($request));
+    }
+
+    protected function throttleKey(Request $request)
+    {
+        return strtolower($request->input($this->username())).'|'.$request->ip();
     }
 }
